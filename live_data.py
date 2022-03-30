1d7b7adbf1d0397b5cd525fbe03f95540ddaf935
Liam Clink <clink.6@osu.edu>
2022-03-30 18:06:44 -0400

    Started working on live plot

--- live_data.py
+++ live_data.py
@@ -2,34 +2,57 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-fig, ax  = plt.subplots(figsize=(11,9))
+class LivePlot():
 
-artists = (ax.plot([], [], animated=True, color='golden')[0],
-           ax.plot([], [], animated=True, color='green')[0],
-           ax.plot([], [], animated=True, color='blue')[0])
+    def __init__(self):
+        self.fig, self.ax  = plt.subplots(figsize=(11,9))
 
-# Shows the plot but doesn't stop the program
-plt.show(block=False)
-plt.pause(0.1)
+        self.ax.set_ylim(0., 2.)
+        self.ax.set_xlim(0., 100.)
 
-# save a copy of the figure before filling it
-background = fig.canvas.copy_from_bbox(fig.bbox)
-for i in range(3):
-    ax.draw_artist(artists[i])
+        self.artists = (self.ax.plot([], [], animated=True, color='gold')[0],
+                        self.ax.plot([], [], animated=True, color='green')[0],
+                        self.ax.plot([], [], animated=True, color='blue')[0])
 
-fig.canvas.blit(fig.bbox)
+        # Shows the plot but doesn't stop the program
+        plt.show(block=False)
+            
+        # Save a copy of the figure before filling it
+        plt.pause(0.1)
+        self.background = self.fig.canvas.copy_from_bbox(self.fig.bbox)
 
-for i in range(100):
-    # Restore canvas to just background
-    fig.canvas.restore_region(background)
-    
-    for j in range(3):
-        artists[j].set_xdata()
-        artists[j].set_ydata()
-        ax.draw_artist(artists[j])
-    
-    # Copy image to GUI state, but may not show yet
-    fig.canvas.blit(fig.bbox)
+        for i in range(len(self.artists)):
+            self.artists[i].set_data(np.zeros(100), np.zeros(100))
+            self.ax.draw_artist(self.artists[i])
 
-    # flush any pending GUI events, re-painting the screen if needed
-    fig.canvas.flush_events()
+        # Copy image to GUI state, but may not show yet
+        self.fig.canvas.blit(self.fig.bbox)
+        # Flush any pending GUI events, paint the window
+        self.fig.canvas.flush_events()
+        
+
+
+    def update(self, x_vals, data):
+        '''
+        Update the plot data and then redraw the lines
+        '''
+
+        # Restore canvas to just background
+        self.fig.canvas.restore_region(self.background)
+
+        for i in range(len(self.artists)):
+            self.artists[i].set_data(x_vals, data[i,:])
+            self.ax.draw_artist(self.artists[i])
+        
+        # Copy image to GUI state, but may not show yet
+        #self.fig.canvas.blit(self.fig.bbox)
+        self.fig.draw()
+
+        # flush any pending GUI events, re-painting the screen if needed
+        self.fig.canvas.flush_events()
+
+
+test = LivePlot()
+
+test.update(np.arange(0,100,1), np.ones((3,100)))
+input()
